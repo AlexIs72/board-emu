b9aec55d031884fd1ba126b4eed1c8f5cb40a997
alexey <alexey@htpc.home>
2018-09-05 13:36:52 +0700

    add stop on error

--- build.mk/targets.mk
+++ build.mk/targets.mk
@@ -1,8 +1,8 @@
 all:
-	@for target in $(TARGETS); do printf "$(CYAN)$(BRIGHT)\n\n   build target: $${target}\n\n$(NORMAL)"; make -C $${target}; done
+	@for target in $(TARGETS); do printf "$(CYAN)$(BRIGHT)\n\n   build target: $${target}\n\n$(NORMAL)"; make -C $${target} || exit 1; done
 
 install:
-	@for target in $(TARGETS); do printf "$(CYAN)$(BRIGHT)\n\n   install target: $${target}\n\n$(NORMAL)"; make -C $${target} install; done
+	@for target in $(TARGETS); do printf "$(CYAN)$(BRIGHT)\n\n   install target: $${target}\n\n$(NORMAL)"; make -C $${target} install || exit 1; done
 
 clean:
-	@for target in $(TARGETS); do printf "$(CYAN)$(BRIGHT)\n\n   clean target: $${target}\n\n$(NORMAL)"; make -C $${target} clean; done
+	@for target in $(TARGETS); do printf "$(CYAN)$(BRIGHT)\n\n   clean target: $${target}\n\n$(NORMAL)"; make -C $${target} clean || exit 1; done
